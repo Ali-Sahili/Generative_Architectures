eca8d2cba9e858e9d49be2d22ab0fb314a55b6ec
Ali-Sahili <60717710+Ali-Sahili@users.noreply.github.com>
2020-04-19 18:58:24 +0200

    New AAE architecture

---
+++ models/AAE2.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+from Param import X_dim, z_dim, Tensor
+
+import numpy as np
+
+def reparameterization(mu, logvar):
+    std = torch.exp(logvar / 2)
+    sampled_z = Variable(Tensor(np.random.normal(0, 1, (mu.size(0), z_dim))))
+    z = sampled_z * std + mu
+    return z
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(X_dim, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.mu = nn.Linear(512, z_dim)
+        self.logvar = nn.Linear(512, z_dim)
+
+    def forward(self, x):
+        x = self.model(x)
+        mu = self.mu(x)
+        logvar = self.logvar(x)
+        z = reparameterization(mu, logvar)
+        return z
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(z_dim, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, X_dim),
+            nn.Tanh(),
+        )
+
+    def forward(self, z):
+        z = self.model(z)
+        return z
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(z_dim, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, z):
+        validity = self.model(z)
+        return validity
+
